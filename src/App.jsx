6e4cde1245547f68db3b64cc9bd5cf840bbf1377
poofsjawn <poofsjawn@gmail.com>
2022-10-16 14:27:51 -0400

    react-vite

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+
+const GltfModel = ({ modelPath, scale = 40, position = [0, 0, 0] }) => {
+  const ref = useRef();
+  const gltf = useLoader(GLTFLoader, modelPath);
+  const [hovered, hover] = useState(false);
+
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.y += 0.003));
+  return (
+    <>
+      <primitive
+        ref={ref}
+        object={gltf.scene}
+        position={position}
+        scale={hovered ? scale * 1.2 : scale}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+      />
+    </>
+  );
+};
+const ModelViewer = ({ modelPath, scale = 40, position = [0, 0, 0] }) => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.3} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Suspense fallback={null}>
+        <GltfModel modelPath={modelPath} scale={scale} position={position} />
+        <OrbitControls />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+    <div style={{ width: "50vw", height: "50vh" }}>
+    <Canvas>
+      <ambientLight intensity={0.5} color="red"/>
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+     
+      <ModelViewer scale="40" modelPath={"/images/island.glb"} />
+
+    </Canvas>
+    </div>
+  )
+}
